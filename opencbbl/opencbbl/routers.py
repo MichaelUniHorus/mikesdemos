60ec0e4b7d45901b99662a277ad4643673ff86e2
Michael Prolubnikov <michael.prolubnikov@yandex.ru>
2026-06-08 22:55:06 +0700

    Fix router to allow auth migrations in all databases

--- opencbbl/opencbbl/routers.py
+++ opencbbl/opencbbl/routers.py
@@ -25,7 +25,7 @@ class AuthRouter:
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         if app_label in self.auth_models:
-            return db == 'default'
+            return True
         return None
 
 
